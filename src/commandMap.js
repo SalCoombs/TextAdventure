17de0d659ebe74b85b23541653ac40ff4818686e
SalCoombs <sal@waldegrave.org>
2025-07-30 10:17:50 -0700

    Added commandMap

--- src/commandMap.js
+++ src/commandMap.js
@@ -1,3 +1,14 @@
-export default class CommandMap {
-  constructor() {}
+const commandMap = {
+  dig: digFn,
+  move: moveFn,
+};
+
+function digFn() {
+  console.log("You are digging");
 }
+
+function moveFn() {
+  console.log("You are moving");
+}
+
+export default commandMap;
