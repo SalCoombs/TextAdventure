9b08cebd7de64a1e5c30dff1bc94a08c08f0eb58
SalCoombs <sal@waldegrave.org>
2025-07-31 16:13:23 -0700

    Figured out event systems

--- src/sandbox.js
+++ src/sandbox.js
@@ -3,3 +3,27 @@
 
 // const match = inputString.match(regex);
 // match.
+
+// const array = [1, 2, 3, 4, 5];
+
+// const result = array.reduce((accum, current) => {
+//   return accum + current;
+// }, 0);
+// console.log(result);
+
+class eventSystem {
+  constructor() {
+    listeners = {};
+  }
+
+  on(type, callback, isPersistant = false) {
+    this.listeners[type] ??= [];
+    this.listeners[type].push();
+  }
+}
+
+class GameEvent {
+  constructor(callback, isPersistant) {
+    this.callback = callback;
+  }
+}
