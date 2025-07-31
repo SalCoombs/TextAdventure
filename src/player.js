d83ee1c3f042b8ea4e1985bfea2f58a748356121
SalCoombs <sal@waldegrave.org>
2025-07-31 16:14:25 -0700

    Added player items.

--- src/player.js
+++ src/player.js
@@ -2,7 +2,15 @@ import Entity from "./entity.js";
 import { entitySymbols } from "./constants.js";
 
 export default class Player extends Entity {
-  constructor() {
+  constructor(eventQueue) {
     super(null, null, null, entitySymbols.PLAYER, 10);
+    this.eventQueue = eventQueue;
+    this.inventory = {};
   }
+
+  addItem(item) {
+    inventory[item.name] = item;
+  }
+
+  dig() {}
 }
